1a0c6d24ec24b5ebcb55456b68e02d08f5e70af3
michel-leonard <101180436+michel-leonard@users.noreply.github.com>
2022-04-26 22:15:55 +0200

    Update qs.c

--- qs.c
+++ qs.c
@@ -210,8 +210,8 @@ positive_number factor(const positive_number number, void *memory) {
         return a ;
     if (is_prime(number, e))
         return number;
-    for(f = 0; f < 4; ++f) {
-        c = factor_rho(number, 1 << 16);
+    for(f = 0; f < 2; ++f) {
+        c = factor_rho(number, 1 << 18);
         if (c != number && c != 1)
             return c ;
     }
